5da28aaf83622fffd8fa1bf6dc88c0502f600598
Jerry Liu <jerrylarryliu@gmail.com>
2020-06-15 10:41:25 -0700

    update

--- project_covid19.py
+++ project_covid19.py
@@ -2,18 +2,29 @@ import os
 import sys
 import time
 import csv
-import paramiko
 import subprocess
-import zipfile
 import tarfile
 import shutil
-from shutil import copytree, ignore_patterns
-import numpy as np
+import numpy
 import stat
+import PIL
+
+
+def delete_folder(path):
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            os.chmod(os.path.join(root, dir), stat.S_IRWXU)
+        for file in files:
+            os.chmod(os.path.join(root, file), stat.S_IRWXU)
+    shutil.rmtree(path, ignore_errors=True)
 
 
 def check_data(repos):
     if not os.path.exists(os.getcwd() + os.sep + "data"):
+
+        for folder in os.listdir(os.getcwd() ):
+            if os.path.isdir(folder) and ".idea" not in folder and ".git" not in folder:
+                delete_folder(os.getcwd() + os.sep + folder)
 
         for repo in repos: os.system("git clone " + repo)
         os.makedirs(os.getcwd() + os.sep + "data")
@@ -22,32 +33,17 @@ def check_data(repos):
             if "twitter" in repo:
                 src = os.getcwd() + os.sep + repo.split('/')[-1].split('.')[0] + os.sep + "dailies"
                 dst = os.getcwd() + os.sep + "data" + os.sep + repo.split('/')[-1].split('.')[0]
-                copytree(src, dst, ignore=ignore_patterns("*.tsv.gz"))
-
-                for root, dirs, files in os.walk(os.getcwd() + os.sep + covid19_twitter_repo.split('/')[-1].split('.')[0]):
-                    for dir in dirs:
-                        os.chmod(os.path.join(root, dir), stat.S_IRWXU)
-                    for file in files:
-                        os.chmod(os.path.join(root, file), stat.S_IRWXU)
-                shutil.rmtree(os.getcwd() + os.sep + covid19_twitter_repo.split('/')[-1].split('.')[0], ignore_errors=True)
-
-
+                shutil.copytree(src, dst, ignore=shutil.ignore_patterns("*.tsv.gz"))
             else:
                 src = os.getcwd() + os.sep + repo.split('/')[-1].split('.')[0] + os.sep + "latest_data"
                 dst = os.getcwd() + os.sep + "data" + os.sep + repo.split('/')[-1].split('.')[0]
-                copytree(src, dst)
+                shutil.copytree(src, dst)
                 for file in os.listdir(dst):
                     if "tar" in file:
                         tar = tarfile.open(dst + os.sep + file, "r:gz")
                         tar.extractall(os.getcwd() + os.sep + "data" + os.sep + "nCoV2019")
                         tar.close()
-
-                for root, dirs, files in os.walk(os.getcwd() + os.sep + nCoV2019_location_repo.split('/')[-1].split('.')[0]):
-                    for dir in dirs:
-                        os.chmod(os.path.join(root, dir), stat.S_IRWXU)
-                    for file in files:
-                        os.chmod(os.path.join(root, file), stat.S_IRWXU)
-                shutil.rmtree(os.getcwd() + os.sep + nCoV2019_location_repo.split('/')[-1].split('.')[0], ignore_errors=True)
+            delete_folder(os.getcwd() + os.sep + repo.split('/')[-1].split('.')[0])
 
 
 if __name__ == "__main__":
