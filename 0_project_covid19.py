ffe3e4122d6e8e2fef795308c4508795cc456bcc
Jerry Liu <jerrylarryliu@gmail.com>
2020-07-30 15:31:39 -0700

    update

--- 0_project_covid19.py
+++ 0_project_covid19.py
@@ -254,20 +254,23 @@ def tweet_daily_topic_modeling(num_topic, token_list, topic_output_path, dominan
         file.close()
 
         output_date_topic_list = []
+        output_date_dominant_list = []
+        input_date_list = []
         for line in output_dates_topic[1:]:
             output_date_topic_list.append(line.split(',')[0])
-
-        output_date_dominant_list = []
         for line in output_dates_dominant[1:]:
             output_date_dominant_list.append(line.split(',')[0])
-
-        input_date_list = []
         for date in list(token_list.keys()):
             input_date_list.append(date)
 
+        date_topic_list = set(input_date_list).difference(set(output_date_topic_list ))
+        date_dominant_list = set(input_date_list).difference(set(output_date_dominant_list))
         for date in list(token_list.keys()):
             if date not in set(input_date_list).difference(set(output_date_topic_list + output_date_dominant_list)):
                 del token_list[date]
+    else:
+        date_topic_list = list(token_list.keys())
+        date_dominant_list = list(token_list.keys())
 
     if not os.path.exists(topic_output_path):
         file = open(topic_output_path, "w+")
@@ -288,45 +291,47 @@ def tweet_daily_topic_modeling(num_topic, token_list, topic_output_path, dominan
         coherence_model_lda = CoherenceModel(model=lda_model, texts=token_list[date], dictionary=dictionary, coherence='c_v')
         coherence_lda = coherence_model_lda.get_coherence()
 
-        topics = lda_model.print_topics(num_words=num_topic)
-        file = open(topic_output_path, "a+")
-        for index, topic in topics:
-            file.write(date + "," + str(index) + "," + str(topic) + "\n")
-            if verbose:  print(date + " " + str(index) + " " + str(topic))
-        file.flush()
-        file.close()
+        if date in date_topic_list:
+            topics = lda_model.print_topics(num_words=num_topic)
+            file = open(topic_output_path, "a+")
+            for index, topic in topics:
+                file.write(date + "," + str(index) + "," + str(topic) + "\n")
+                if verbose:  print(date + " " + str(index) + " " + str(topic))
+            file.flush()
+            file.close()
 
-        sent_topics_df = pd.DataFrame()
-        for i, row_list in enumerate(lda_model[corpus]):
-            row = row_list[0] if lda_model.per_word_topics else row_list
-            row = sorted(row, key=lambda x: (x[1]), reverse=True)
-            for j, (topic_num, prop_topic) in enumerate(row):
-                if j == 0:
-                    wp = lda_model.show_topic(topic_num)
-                    topic_keywords = ", ".join([word for word, prop in wp])
-                    sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic, 4), topic_keywords]), ignore_index=True)
-                else:
-                    break
-        sent_topics_df.columns = ['topic_index', 'percent_contribution', 'topic_keywords']
-        file = open(dominant_output_path, "a+")
-        data = list(sent_topics_df.values.tolist())
-        data.sort(key=lambda x: x[1], reverse=True)
-        data = data[0]
-        data[-1] = data[-1].replace(',', ';').replace(' ','')
-        file.write(date + ',' + str(lda_model.log_perplexity(corpus)) + ',' + str(coherence_lda) + ',' + ','.join([str(x) for x in data]) + '\n')
-        file.flush()
-        file.close()
-        if verbose: print(date, lda_model.log_perplexity(corpus), coherence_lda, data)
+        if date in date_dominant_list:
+            sent_topics_df = pd.DataFrame()
+            for i, row_list in enumerate(lda_model[corpus]):
+                row = row_list[0] if lda_model.per_word_topics else row_list
+                row = sorted(row, key=lambda x: (x[1]), reverse=True)
+                for j, (topic_num, prop_topic) in enumerate(row):
+                    if j == 0:
+                        wp = lda_model.show_topic(topic_num)
+                        topic_keywords = ", ".join([word for word, prop in wp])
+                        sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic, 4), topic_keywords]), ignore_index=True)
+                    else:
+                        break
+            sent_topics_df.columns = ['topic_index', 'percent_contribution', 'topic_keywords']
+            file = open(dominant_output_path, "a+")
+            data = list(sent_topics_df.values.tolist())
+            data.sort(key=lambda x: x[1], reverse=True)
+            data = data[0]
+            data[-1] = data[-1].replace(',', ';').replace(' ','')
+            file.write(date + ',' + str(lda_model.log_perplexity(corpus)) + ',' + str(coherence_lda) + ',' + ','.join([str(x) for x in data]) + '\n')
+            file.flush()
+            file.close()
+            if verbose: print(date, lda_model.log_perplexity(corpus), coherence_lda, data)
 
 
 if __name__ == "__main__":
-    # print_header(os.getcwd() + os.sep + "header.txt")
-    #
-    # print("Loading NLTK Data Packages...")
-    # load_nltk_packages()
-    #
-    # print("Checking for dataset updates...")
-    # dataset_check()
+    print_header(os.getcwd() + os.sep + "header.txt")
+
+    print("Loading NLTK Data Packages...")
+    load_nltk_packages()
+
+    print("Checking for dataset updates...")
+    dataset_check()
 
     nlp = spacy.load("en")
     words = set(nltk.corpus.words.words())
@@ -341,23 +346,23 @@ if __name__ == "__main__":
     tweet_training_data_path = os.getcwd() + os.sep + "data" + os.sep + "training_data_t4sa" + os.sep + "training_data_t4sa.csv"
     tweet_classifier_model_path = os.getcwd() + os.sep + "data" + os.sep + "classifier" + os.sep + "NaiveBayesClassifier_1M.pickle"
 
-    # print("Loading COVID-19 related datasets...")
-    # tweet_data = load_csv_data(tweet_filtered_data_directory)
-    #
-    # print("Creating Data Frames...")
-    # tweet_data_frame = to_data_frame(tweet_data)
-    #
-    # print("Training/Loading Tweet Classifier Model...")
-    # classifier = create_classifier_model(tweet_training_data_path, tweet_classifier_model_path, 9999999)
-    #
-    # print("Testing Classifier Model on Example Tweets...")
-    # validate_classifier(classifier)
-    #
-    # print("Processing Sentiment Analyzer...")
-    # tweet_sentiment_analyzer(tweet_data, classifier, tweet_sentiment_result_directory, verbose=False)
+    print("Loading COVID-19 related datasets...")
+    tweet_data = load_csv_data(tweet_filtered_data_directory)
+
+    print("Creating Data Frames...")
+    tweet_data_frame = to_data_frame(tweet_data)
+
+    print("Training/Loading Tweet Classifier Model...")
+    classifier = create_classifier_model(tweet_training_data_path, tweet_classifier_model_path, 9999999)
+
+    print("Testing Classifier Model on Example Tweets...")
+    validate_classifier(classifier)
+
+    print("Processing Sentiment Analyzer...")
+    tweet_sentiment_analyzer(tweet_data, classifier, tweet_sentiment_result_directory, verbose=False)
 
     print("Processing Topic Modeling...")
     tweet_tokens_daily, tweet_tokens_total = load_tweet_tokens(tweet_tokenized_directory)
     tweet_daily_topic_modeling(10, tweet_tokens_daily, tweet_topic_modeling_result_daily_directory, tweet_dominant_topic_result_daily_directory, verbose=True)
 
-    # os.system("python " + os.getcwd() + os.sep + "5_data_visualization.py")
+    os.system("python " + os.getcwd() + os.sep + "5_data_visualization.py")
